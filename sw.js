6eb21f958e7495b420bcc6ff0ff64576595839b5
FAVU <guilherme_adgg@hotmail.com>
2026-06-09 21:38:18 -0300

    pedidos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'favu-admin-v1';
+const CACHE_NAME = 'favu-admin-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,6 +9,20 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
